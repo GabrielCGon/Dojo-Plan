3b0ff451f669d19c8aec359caf9a188a2d874870
gabrielCGon <gabriel_costa1905@outlook.com>
2022-07-05 14:41:31 -0400

    Update dojo-1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
@@ -40,11 +18,6 @@ import HelloWorld from "@/components/HelloWorld.vue";
 header {
   line-height: 1.5;
   max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 a,
@@ -97,12 +70,6 @@ nav a:first-of-type {
     padding: 0 2rem;
   }
 
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
   header .wrapper {
     display: flex;
     place-items: flex-start;
@@ -111,15 +78,6 @@ nav a:first-of-type {
 
   .logo {
     margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <img src="src/assets/image 4.png" alt="" />
+  </main>
+</template>
